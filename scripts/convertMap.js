74e23033433efd28e1c4b71257d4a7b9bcbe664d
Dmitry Pustovit <dpustovi@uncc.edu>
2017-04-03 04:29:22 +0000

    Bug Fixes

---
+++ scripts/convertMap.js
@@ -0,0 +1,40 @@
+function run(q){
+    if( q == 1)
+        map(document.getElementById("mapData").value);
+            
+    if( q == 2)
+    {
+        $.get( "./out.txt", function( data ) {
+            console.log("Data!");
+        })
+        .done(function(data) {
+        map(data);
+        });
+    }
+}
+
+function map(a){
+    var form = document.getElementById("dataForm").style.visibility = "hidden"; 
+    
+    document.getElementById("map").style.display = "block"; 
+            
+    var rawMap = a;
+    var mapArray = new Array();
+    
+    mapArray = rawMap.split(" ");
+            
+    var canvas = document.getElementById("map").getContext('2d');
+    var imgData = canvas.createImageData(864, 635904 / 864);
+            
+    for(var i = 0; i < imgData.data.length; i+= 4)
+    {
+        imgData.data[i] = 149;
+        imgData.data[i+1] = 165;
+        imgData.data[i+2] = 166;
+        imgData.data[i+3] = 255 - mapArray[i/4];
+            
+    }
+            
+    canvas.putImageData(imgData, 0, 0);   
+}
+
