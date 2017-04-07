4946fc07ab1014df7a9fd6365b1037d0b1bbb040
Dmitry Pustovit <me@dmitrypustovit.com>
2017-04-06 20:01:07 -0400

    Added to Compression

--- PythonAssets/imgDataCompression.py
+++ PythonAssets/imgDataCompression.py
@@ -6,7 +6,7 @@ var = pgm("roomba_hall.pgm")
 x = 0
 
 while x < len(var.bytes):
-    if str(var.bytes[x]) != "205":
+    if str(var.bytes[x]) != "205" and str(var.bytes[x]) != "254":
         f.write(str(var.bytes[x]) + " ")
         x += 1
     elif str(var.bytes[x]) == "205":
@@ -17,9 +17,21 @@ while x < len(var.bytes):
             else:
                 break
             
-        f.write('[' + str(counter - x) + ']' + ' ')
+        f.write('A' + str(counter - x) + ' ')
         x = counter + 1
-    
+        
+    elif str(var.bytes[x]) == "254":
+        counter = x
+        while str(var.bytes[counter]) == "254":
+            if(counter < len(var.bytes) - 1):
+                counter += 1
+            else:
+                break
+            
+        f.write('B' + str(counter - x) + ' ')
+        x = counter + 1
+
+        
 print len(var.bytes)
 print var.width
 print "DONE"
